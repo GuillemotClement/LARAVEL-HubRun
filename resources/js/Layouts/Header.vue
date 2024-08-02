cbbf7453c291b265fd795364c060bef701c24803
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-02 22:16:15 +0200

    Ajout du lien CDN Font Awesome dans app.blade.php

---
+++ resources/js/Layouts/Header.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+<template>
+    <div class="bg-slate-500 py-5">
+        <div class="">
+            <img src="" alt="" />
+            <p>HubRun</p>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Utils.vue
+++ resources/js/Pages/Utils.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
+import Header from "@/Layouts/Header.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
 import { ref } from "vue";
 const name = ref("");
 </script>
 <template>
-    <p>Hello {{ name }}</p>
-    <input type="text" v-model="name" class="" />
+    <Head title="HubRun" />
+    <Header />
+    <div class="container mx-auto min-h-screen border border-red-500">
+        <p>Hello {{ name }}</p>
+        <input type="text" v-model="name" class="rounded-xl" />
+    </div>
 </template>
