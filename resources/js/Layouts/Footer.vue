ee0ed29151bacb8f57386ae945758d13f99f65d3
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-02 22:23:37 +0200

    Ajout du footer avec date dynamique

---
+++ resources/js/Layouts/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const date = new Date();
+const year = date.toLocaleString("fr-FR", { year: "numeric" });
+</script>
+<template>
+    <footer class="p-5">
+        <p>Copyright &copy; {{ year }}</p>
+    </footer>
+</template>
--- resources/js/Pages/Utils.vue
+++ resources/js/Pages/Utils.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import Header from "@/Layouts/Header.vue";
+import Footer from "@/Layouts/Footer.vue";
 import { Head, Link } from "@inertiajs/vue3";
 
 import { ref } from "vue";
@@ -12,4 +13,5 @@ const name = ref("");
         <p>Hello {{ name }}</p>
         <input type="text" v-model="name" class="rounded-xl" />
     </div>
+    <Footer />
 </template>
